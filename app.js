3ae7631ffa8ff408bbd7e125e7c6eb97e30f77ef
promise310 <seoyoung22dev@gmail.com>
2023-02-01 18:03:09 -0500

    Practice

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hi");
+const a = 5;
+const b = 6;
+let myName = "nico";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hi " + myName);
+
+myName = "nicolas"
+
+console.log("Your new name is " + myName);
+
