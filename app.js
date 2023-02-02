3ff39e41af7e83f18c6e581c477166b7946b9001
promise310 <seoyoung22dev@gmail.com>
2023-02-02 15:47:52 -0500

    Events

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-const age = parseInt(prompt("How old are you?"));
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.log(age);
+console.dir(h1);
+
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function hadleWindowCopy(){
+    alert("Copier!");
+}
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", hadleWindowCopy);
 
