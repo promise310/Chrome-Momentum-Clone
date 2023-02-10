13764463d8bf99baedc0ecfbffad3dfdc1e0ab16
promise310 <seoyoung22dev@gmail.com>
2023-02-09 22:00:48 -0500

    Clock

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock (){
+    //get date information
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    
+    clock.innerHTML =`${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+
+//call getClock every second
+setInterval(getClock, 1000);
--- js/greetings.js
+++ js/greetings.js
@@ -7,28 +7,35 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
+    //stop the event refreshing the page
     event.preventDefault();
+    //hide the form again
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    //save the value of the login input in a variable called userName
     const userName = loginInput.value;
-    //Combinig Strings
-    //greeting.innerText = "Hello " + userName;
-
-    //saving username to local storage
+    //save the username on the local storage under the key username
     localStorage.setItem(USERNAME_KEY, userName);
+    //call the function
     paintGreetings(userName);
     
 }
 
 function paintGreetings(userName){
+    //Combinig Strings
+    //greeting.innerText = "Hello " + userName;
     greeting.innerText = `Hello ${userName}`;
+    //remove the hidden class name title from the greeting title
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+//get username from the API local storage
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
     //show the form
+    //remove the hidden class name from thr html login-form
     loginForm.classList.remove(HIDDEN_CLASSNAME);
+    //addEventListener waiting for submit
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     //show the greeting
