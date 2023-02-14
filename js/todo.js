8a309c3dbaaee9d7e0a3e8fda214e584f03c8826
promise310 <seoyoung22dev@gmail.com>
2023-02-14 00:36:15 -0500

    Todo update

--- js/todo.js
+++ js/todo.js
@@ -2,22 +2,28 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
-const todos = [];
+const TODOS_KEY = "todos";
+let todos = [];
+
 
 function saveTodos(){
-    localStorage.setItem("todos", JSON.stringify(todos));
+    //JSON.stringify -> save value as a string
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodos();
 }
 
 function paintTodo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
     //change the text of the span
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     //change the text of the button
     button.innerText = "❌";
@@ -35,10 +41,32 @@ function handelTodoSubmit(event){
     //coping the current value of the input into a new variable
     const newTodo = todoInput.value;
     todoInput.value = "";
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
     //text inside the todos array
-    todos.push(newTodo);
+    todos.push(newTodoObj);
     //paint on the screen
-    paintTodo(newTodo);
+    paintTodo(newTodoObj);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", handelTodoSubmit);
+
+// function sayHello(item) {
+//     console.log("this is the turn of", item);
+// }
+
+
+//turn string into array
+ const savedTodos = localStorage.getItem(TODOS_KEY);
+
+ if(savedTodos !== null) {
+    const parsedTodos = JSON.parse(savedTodos);
+
+    todos = parsedTodos;
+    //execute one function for each item on the array
+    //parsedTodos.forEach((item) => console.log("this is the turn of",item));
+    parsedTodos.forEach(paintTodo);
+ }
