6e89ded31cc06bc99a9612b7651e2ba30839868d
promise310 <seoyoung22dev@gmail.com>
2023-03-03 20:59:12 -0500

    Changed img file

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "8.png"];
 
 //get a random number based on my array length
 const chosenImg = images[Math.floor(Math.random()*images.length)];
