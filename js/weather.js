eaf6dc7a615bd9e54d7a5e166f0cd55c40e57d08
promise310 <seoyoung22dev@gmail.com>
2023-03-07 02:59:32 -0500

    Update

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,6 @@
-const weather = document.querySelector("#weather span:first-child ");
-const city = document.querySelector("#weather span:last-child ");
+const city = document.querySelector("#weather span:first-child ");
+const weather = document.querySelector("#weather span:last-child ");
+
 const API_KEY = "17ee502d52187dd0afce354f31851a90";
 
 
@@ -13,7 +14,8 @@ function onGeoOk(position) {
     .then((response) => response.json())
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      //const ftemp = (data.main.ctemp * 9 / 5) +32;
+      weather.innerText = `${data.weather[0].main}  ${data.main.temp}°C`;
     });
 }
 
